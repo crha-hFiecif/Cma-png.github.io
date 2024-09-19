89067eb8a1d91a903abc921ed63a0a40ff94016a
Cma-png <147258051+Cma-png@users.noreply.github.com>
2024-09-19 16:08:32 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@
  */
 const nextConfig = {
   output: 'export',
+  basePath: '/Cma-png.github.io',  // Replace with your actual repository name
+  assetPrefix: '/Cma-png.github.io/',  // Replace with your actual repository name
   images: {
-    unoptimized: true, // Disable the image optimization
+    unoptimized: true,
   },
 };
 
